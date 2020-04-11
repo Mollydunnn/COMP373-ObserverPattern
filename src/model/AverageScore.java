f8ae7dd7c93b21e60f7b4b303b6609a0218de56e
nidahameed <nidahameed@192.168.0.14>
2020-04-10 19:16:05 -0500

    finished adding methods to AverageScore, still need to get data in dal

--- src/model/AverageScore.java
+++ src/model/AverageScore.java
@@ -2,11 +2,25 @@ package model;
 
 public class AverageScore implements Observer {
 
-	@Override
+	private float twoPointerRate;
+	private float threePointerRate;
+	private float freeThrowRate;
+	
+	
 	public void update(int twoPointersMade, int threePointersMade, int freeThrowsMade, int twoPointersAttempted,
 			int threePointersAttempted, int freeThrowsAttempted) {
 		// TODO Auto-generated method stub
-		
+		this.twoPointerRate = ((twoPointersMade/twoPointersAttempted)*100);
+		this.threePointerRate = ((threePointersMade/threePointersAttempted)*100);
+		this.twoPointerRate = ((freeThrowsMade/freeThrowsAttempted)*100);
+		display();
+	}
+	
+	public void display() {
+		System.out.println("Two Pointer Stats: " + twoPointerRate + "% of the two pointers attempted were successfully");
+		System.out.println("Three Pointer Stats: " + threePointerRate + "% of the three pointers attempted were successfully");
+		System.out.println("Free Throw Stats: " + freeThrowRate + "% of the free throws attempted were successfully");
+
 	}
 
 }
